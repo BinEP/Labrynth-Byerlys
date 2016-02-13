64754ab3442fceaabbc5c0e174e85a5f32ee4895
Brady <bradystoffel@gmail.com>
2016-02-13 14:55:37 -0600

    Controllers Added

---
+++ src/controllers/BuilderPoints.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import extendeds.Controller;
+
+public class BuilderPoints extends Controller {
+	public static void main(String[] args) {
+
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean shouldEndGame() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setup() {
+		// TODO Auto-generated method stub
+
+	}
+}
