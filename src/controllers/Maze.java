e0b7574eb5bf1ec42ba79f17913e7abac71632e8
ParkerZ <pz7air@gmail.com>
2016-02-13 16:13:10 -0600

    Added Maze Drawing

--- src/controllers/Maze.java
+++ src/controllers/Maze.java
@@ -1,10 +1,31 @@
 package controllers;
 
+import java.awt.Color;
+
+import shapes.BSRectangle;
 import extendeds.Controller;
 
 public class Maze extends Controller {
-	public static void main(String[] args) {
+	public int xStartCoord = 200;
+	public int yStartCoord = 0;
+	public int tileSize = 40;
+	public int mapSide = 400;
+	
+	public BSRectangle mapSize = new BSRectangle(xStartCoord, yStartCoord, mapSide, mapSide);
+	public int[][] map = 
+		   {{0, 1, 1, 0, 0, 0, 1, 0, 0, 0}, 
+			{0, 0, 0, 1, 1, 0, 1, 0, 1, 1}, 
+			{0, 1, 0, 1, 1, 0, 1, 0, 0, 1}, 
+			{0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 1, 1, 0, 1, 0, 1, 0, 1, 0}, 
+			{0, 1, 0, 0, 0, 0, 1, 0, 1, 1}, 
+			{1, 1, 0, 1, 0, 1, 1, 0, 0, 0}, 
+			{0, 0, 0, 1, 0, 0, 1, 1, 1, 0}, 
+			{0, 1, 0, 1, 1, 0, 1, 0, 1, 0}, 
+			{0, 0, 0, 1, 0, 0, 1, 0, 0, 0},};
 
+	public static void main(String[] args) {
+		
 	}
 
 	@Override
@@ -28,6 +49,21 @@ public class Maze extends Controller {
 	@Override
 	public void setup() {
 		// TODO Auto-generated method stub
+		mapSize.setColor(Color.WHITE);
+		controller.addShapeToBeDrawn(mapSize);
+		for (int i = 0; i < map.length; i++){
+			for (int j = 0; j < map[i].length; j++){
+				BSRectangle square = new BSRectangle(xStartCoord + tileSize * i, tileSize * i, tileSize, tileSize);
+				if (map[i][j] == 1){
+					//setColor(Color.WHITE);
+					square.setColor(Color.BLACK);
+					controller.addShapeToBeDrawn(square);
+				} else {
+					square.setColor(Color.WHITE);
+					controller.addShapeToBeDrawn(square);
+				}
+			}
+		}
 
 	}
 }
