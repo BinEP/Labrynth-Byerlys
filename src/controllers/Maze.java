86bed639d0eda0d44429229d1a6ee67b217b798a
ParkerZ <pz7air@gmail.com>
2016-02-14 08:25:37 -0600

    Added some drawings to Maze

--- src/controllers/Maze.java
+++ src/controllers/Maze.java
@@ -4,6 +4,9 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.util.ArrayList;
 
+import shapes.BSCircle;
+import shapes.BSCompound;
+import shapes.BSPolygon;
 import shapes.BSRectangle;
 import shapes.interfaces.BSShape;
 import extendeds.Controller;
@@ -96,14 +99,43 @@ public class Maze extends Controller implements NetworkListener, NetworkVariable
 		
 	for (int i = 0; i < map.length; i++){
 		for (int j = 0; j < map[i].length; j++){
+			
+			BSRectangle bananaStem = new BSRectangle(200 + 40 * j + 28, 40 * i + 14, 5, 7);
+			int[] bananaX = 
+				{200 + 40 * j + 12, 200 + 40 * j + 22, 200 + 40 * j + 30, 200 + 40 * j + 38, 200 + 40 * j + 48, 200 + 40 * j + 42,
+					200 + 40 * j + 32, 200 + 40 * j + 27, 200 + 40 * j + 18, 560 + 12};
+			int[] bananaY = 
+				{40 * i  + 40, 40 * i + 37, 40 * i + 27, 40 * i + 37, 40 * i + 40, 40 * i + 35, 40 * i + 20, 40 * i + 20, 40 * i  + 53,
+					40 * i + 40};
+			BSPolygon bananaPeel = new BSPolygon(bananaX, bananaY, bananaX.length);
+			BSCompound banana;
+			bananaStem.setColor(Color.GREEN);
+			bananaPeel.setColor(Color.YELLOW);
+			
+			BSCircle puddleA = new BSCircle(200 + 40 * j + 10, 40 * i + 10, 20);
+			BSCircle puddleB = new BSCircle(200 + 40 * j + 20, 40 * i + 18, 30);
+			BSCircle puddleC = new BSCircle(200 + 40 * j + 25, 40 * i + 7, 20);
+			puddleA.setColor(Color.BLUE);
+			puddleB.setColor(Color.BLUE);
+			puddleC.setColor(Color.BLUE);
+			
 			BSRectangle square = new BSRectangle(xStartCoord + tileSize * j, tileSize * i, tileSize, tileSize);
 			square.setColor(Color.WHITE);
 
 			if (state.map[i][j] == 0 && (state.runnerVision.intersects(square.getBounds2D()) || !main.role.role)) {
 				square.setColor(Color.GRAY);
+				square.autoDraw(g);
+			} else if (state.map[i][j] == 2 || !main.role.role) {
+				bananaStem.autoDraw(g);
+				bananaPeel.autoDraw(g);
+			} else if (state.map[i][j] == 2 || !main.role.role) {
+				puddleA.autoDraw(g);
+				puddleB.autoDraw(g);
+				puddleC.autoDraw(g);
+			} else {
+				square.setColor(Color.WHITE);
+				square.autoDraw(g);
 			}
-			
-			square.autoDraw(g);
 		}
 	}
 		
